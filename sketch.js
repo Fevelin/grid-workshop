b65593463c780efe1cb2970510fd9392959f08e7
Fevelin <evelin.flamis@student.norwichuni.ac.uk>
2024-02-07 09:49:21 +0000

    02/02/24

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
 const textures = [];
+const colorArray = ["#f1930b", "#f6e272", "#900945", "#aeaf64", "#80e33a ", "#5b3c44"];
 
 function preload() {
-
-    // loop trough the 4 textures that we have saved
-    for(let textureNumber = 1; textureNumber <= 4; textureNumber++){
-        //get the path of the current texture
-        const pathToTexture = 'Textures/texture-trans' + textureNumber + '.png';
-        //load the image using the loadImage p5.js function and add it to the texture list
-        textures.push(loadImage(pathToTexture));
-    }
 }
 
 
@@ -17,39 +10,31 @@ function setup() {
     //size of each square in pixels
     const squareSize = 50;
     //number of cells of the gric
-    const cellNumber = 10;
-    //canvas size in pixels
-    const canvasSize = squareSize * cellNumber;
+    const cellNumber = 20;
     //maximum offset
-    const maxOffsetPosition = 2;
-    const maxOffsetSize = 5;
+    const maxOffsetPosition = 150;
+    //const maxOffsetSize = 150;
+    const possibleCellsizes = [2 * squareSize, 3 * squareSize, 5 * squareSize];
 
-    createCanvas(canvasSize, canvasSize);
-    background(220);
+    createCanvas(800, 800);
+    noStroke();
+    background(205, 92, 92);
 
     for(let row = 0; row < cellNumber; row++) {
         for(let column = 0; column < cellNumber; column++) {
-            const red = random(255);
-            const green = random(255);
-            const blue = random(255);
 
-            const myColor = color(red, green, blue);
-            // get random texture
-            const texture = random(textures);
+           const cellColor = random(colorArray);
 
             // use fill to fill all the square with one color
-            //fill(myColor)
-
-            //tint the square
-            tint(myColor)
+            fill(cellColor);
 
             //calculate random offset
             const randomOffsetHorizontal = random(-maxOffsetPosition, maxOffsetPosition);
             const randomOffsetVertical = random(-maxOffsetPosition, maxOffsetPosition);
 
             //random offset with size
-            const randomOffsetHeight = random(-maxOffsetSize, maxOffsetSize);
-            const randomOffsetWidth = random(-maxOffsetSize, maxOffsetSize);
+            //const randomOffsetHeight = random(-maxOffsetSize, maxOffsetSize);
+            //const randomOffsetWidth = random(-maxOffsetSize, maxOffsetSize);
 
             //calculate horizontal position in pixels
             const x = row * squareSize + randomOffsetHorizontal;
@@ -57,15 +42,18 @@ function setup() {
             const y = column * squareSize + randomOffsetVertical;
 
             //set witdh  and height
-            const width = squareSize + randomOffsetWidth;
-            const height = squareSize + randomOffsetHeight;
+            //const width = squareSize + randomOffsetWidth;
+            //const height = squareSize + randomOffsetHeight;
+
+            const width= random(possibleCellsizes);
+            const height = random(possibleCellsizes);
 
 
           
 
             //image(background x position, y position, width, height)
             //will draw image the given background at the given position and width/height
-            image(texture, x, y, width, height)
+            rect(x, y, width, height)
 
             //square(x, y, s)
             //x = position of the left of the square
